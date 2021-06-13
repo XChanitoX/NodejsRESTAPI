cb4e2aefc77635fc244d65277f7db5b22d298b87
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-06-13 03:29:48 -0500

    Agregando metodos PostMan

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ const app = express()
 app.set("pkg", pkg);
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
@@ -19,6 +20,6 @@ app.get('/', (req, res) => {
     })
 })
 
-app.use(productsRoutes)
+app.use('/products', productsRoutes)
 
 export default app;
